843fb6da4f04fd3396818562b494e90ed3878f7b
Alexey Chugunov <57045051+alex-chugunov@users.noreply.github.com>
2020-09-19 20:35:59 +0300

    try2

--- task6.py
+++ task6.py
@@ -1,1 +1,8 @@
 st = input("Введите число: ")
+n = int(st[-2:])
+if n % 10 == 0 or n % 10 in range(5,10) or n // 10 == 1:
+     print(st + " программистов")
+elif n % 10 == 1:
+     print(st + " программист")
+else:
+     print(st + " программиста")
